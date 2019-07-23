5683bd1df5e30d882f83b9594343264619c74546
marianneji <marianneji@gmail.com>
2019-07-23 15:41:45 +0200

    Application simple, tableView, UIAlert, addTExtField

--- ShoppingApp/ViewController.swift
+++ ShoppingApp/ViewController.swift
@@ -8,12 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startShoppingList))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItemList))
+        
+        
     }
+    
+    @objc func startShoppingList() {
+        title = "ShoppingList"
+        shoppingList.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    @objc func addItemList() {
+        let ac = UIAlertController(title: "Enter your item to your shopping list", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submitItem(answer)
+            
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submitItem(_ item: String) {
+        shoppingList.insert(item.capitalized, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row].capitalized
+        return cell
+    }
+    
+    
 
 
 }
